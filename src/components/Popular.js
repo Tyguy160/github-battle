27b64de2bb2438633f741600cd250d9e26ea3972
Tyler Stephen Maschino <tylermaschino@gmail.com>
2018-04-29 09:05:02 -0400

    Updated Popular.js with separate components

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,4 +1,31 @@
 var React = require("react");
+var PropTypes = require("prop-types");
+
+function SelectLanguage(props) {
+  var languages = ["All", "Javascript", "Ruby", "Java", "CSS", "Python"];
+  return (
+    <ul className="languages">
+      {languages.map(lang => {
+        return (
+          <li
+            key={lang}
+            onClick={props.onSelect.bind(null, lang)}
+            style={
+              lang === props.selectedLanguage ? { color: "#d0021b" } : null
+            }
+          >
+            {lang}
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+SelectLanguage.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired
+};
 
 class Popular extends React.Component {
   constructor(props) {
@@ -18,25 +45,13 @@ class Popular extends React.Component {
     });
   }
   render() {
-    var languages = ["All", "Javascript", "Ruby", "Java", "CSS", "Python"];
     return (
-      <ul className="languages">
-        {languages.map(lang => {
-          return (
-            <li
-              key={lang}
-              onClick={this.updateLanguage.bind(null, lang)}
-              style={
-                lang === this.state.selectedLanguage
-                  ? { color: "#d0021b" }
-                  : null
-              }
-            >
-              {lang}
-            </li>
-          );
-        })}
-      </ul>
+      <div>
+        <SelectLanguage
+          selectedLanguage={this.state.selectedLanguage}
+          onSelect={this.updateLanguage}
+        />
+      </div>
     );
   }
 }
